9b1c5fe4fca8cab79625b72cfd6edd70f5406f5e
Dzikri Aziz <kvcrvt@gmail.com>
2014-02-05 17:09:51 +0700

    Bump version to 0.1.3
    This is needed so browsers will download the new css file

--- tinycoffee.php
+++ tinycoffee.php
@@ -3,7 +3,7 @@
  * Plugin Name: tinyCoffee
  * Plugin URI: http://github.com/ideag/tiny_coffee
  * Description: Ask people for coffee money
- * Version: 0.1.2
+ * Version: 0.1.3
  * Author: Arūnas Liuiza
  * Author URI: http://github.com/ideag
  * Text Domain: tinycoffee
@@ -22,7 +22,7 @@ if ( ! function_exists( 'add_action' ) ) {
  */
 class Tiny_Coffee {
 
-	const VERSION = '0.1.2';
+	const VERSION = '0.1.3';
 
 	/**
 	 * Holds status
